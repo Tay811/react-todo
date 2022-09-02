88d5d3e229949c4c30bb9bdd637ecfa75793a27f
Tay811 <mataitaya@gmail.com>
2022-09-02 22:14:53 +0300

    todo2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,9 @@ import React from "react"
 function Header({name}){
     return <nav className="header-main">
         <div className="header-wrapper">
+            <ul className="header-span">
+                <span>Ваш никнейм: {name}</span>
+            </ul>
         </div>
     </nav>
 }
