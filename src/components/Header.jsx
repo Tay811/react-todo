c0d9b4ca3dabec6aa3772c1ffc9174d90a896dba
Tay811 <mataitaya@gmail.com>
2022-09-02 21:56:34 +0300

    todo

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+function Header({name}){
+    return <nav className="header-main">
+        <div className="header-wrapper">
+        </div>
+    </nav>
+}
+
+export {Header}
